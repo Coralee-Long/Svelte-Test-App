2cc2c6e5321aa9fcf2d21ff75adfcb9fd96bd031
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2022-05-26 16:24:11 +0200

    test connection

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+	export let age;
+
+	const incrementAge = () => age +=1;
+</script>
+
+<style>
+
+	h1 {
+		color: purple;
+	}
+
+</style>
+
+<main>
+	<h1>Hello, I'm {name}, my age is {age}!</h1>
+	<button on:click={incrementAge}>Change Age</button>
+</main>
+
